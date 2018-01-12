3538d852685b4a186ee4a2be09cfb3dc26898409
Lorenzo Lombardi <l.lombardi@afbnet.it>
2018-01-12 18:01:35 +0100

    Remove of F7 v1

--- www/js/common.js
+++
@@ -1,8 +0,0 @@
-function loadCommonBahaviors(){
-    $$('.backlink').on('click', function(e){
-        var backurl = $$(this).attr('data-back');
-        // mainView.router.back();
-        mainView.router.load({pageName: backurl});
-
-    });
-}
